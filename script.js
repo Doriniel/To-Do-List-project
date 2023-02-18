9f879adfacfca3d413488cc4f3fc19fdc3e179bb
Doriniel <doriniel@gmail.com>
2023-02-18 21:21:45 +0300

    refactor: delete old comments

--- script.js
+++ script.js
@@ -1,232 +1,112 @@
 // План действий:
 // 1) Внизу списка находится кнопка «Добавить». При нажатии на неё в конец списка добавляется новая пустая запись.
-// 2) Над списком присутствует кнопка сортировки. При нажатии на неё записи сортируются по алфавиту. 
-// 3) Повторное нажатие приводит к сортировке по алфавиту в обратном порядке. 
-// 4) После каждого нажатия кнопка меняет свое состояние, показывая пользователю, 
+// 2) Над списком присутствует кнопка сортировки. При нажатии на неё записи сортируются по алфавиту.
+// 3) Повторное нажатие приводит к сортировке по алфавиту в обратном порядке.
+// 4) После каждого нажатия кнопка меняет свое состояние, показывая пользователю,
 // в каком порядке будет происходить сортировка (прямом или обратном).
 // 5) В начальном состоянии список содержит одну пустую запись.
 // 6) drag and drop.
 
-let array = [''];
+let array = [""];
 
-const tasker = document.querySelector('.tasker');
-const firstTask = document.querySelector('.first-task');
-const otherTasks = document.querySelector('.other-tasks');
-let task = document.querySelector('.task-input');
-const addTaskButton = document.querySelector('.button-add');
-const sort = document.querySelector('.sort');
+const tasker = document.querySelector(".tasker");
+const firstTask = document.querySelector(".first-task");
+const otherTasks = document.querySelector(".other-tasks");
+let task = document.querySelector(".task-input");
+const addTaskButton = document.querySelector(".button-add");
+const sort = document.querySelector(".sort");
 
 renderList();
 
-addTaskButton.addEventListener('click', addHandler);
+addTaskButton.addEventListener("click", addHandler);
 
-sort.addEventListener('click', sortButtonChange);
-
+sort.addEventListener("click", sortButtonChange);
 
 function renderList() {
-    otherTasks.innerHTML = '';
-    array.forEach((item, index) => {
-        otherTasks.append(createTaskElement(item, index));
-    });
+  otherTasks.innerHTML = "";
+  array.forEach((item, index) => {
+    otherTasks.append(createTaskElement(item, index));
+  });
 }
 
-
 function sortButtonChange(event) {
-    event.target.classList.toggle('sort-up'); //здесь при нажатии на кнопку сортировки присваиваем ей каждый раз новый класс (.toggle)- если есть, то меняет на другой.
+  event.target.classList.toggle("sort-up"); //здесь при нажатии на кнопку сортировки присваиваем ей каждый раз новый класс (.toggle)- если есть, то меняет на другой.
 
-    if (event.target.classList.contains('sort-up')) {
-        sortHandlerAscending();
-    } else {
-        sortHandlerDescending();
-    }
-};
-
+  if (event.target.classList.contains("sort-up")) {
+    sortHandlerAscending();
+  } else {
+    sortHandlerDescending();
+  }
+}
 
 function sortHandlerAscending() {
-    array.sort((a, b) => {
-        if (a < b) {
-            return -1;
-        }
-        if (a > b) {
-            return 1;
-        }
-        if (a === b) {
-            return 0;
-        }
-    })
-    renderList();
+  array.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+    if (a === b) {
+      return 0;
+    }
+  });
+  renderList();
 }
 
 function sortHandlerDescending() {
-    array.sort((a, b) => {
-        if (a > b) {
-            return -1;
-        }
-        if (a < b) {
-            return 1;
-        }
-        if (a === b) {
-            return 0;
-        }
-    })
-    renderList();
+  array.sort((a, b) => {
+    if (a > b) {
+      return -1;
+    }
+    if (a < b) {
+      return 1;
+    }
+    if (a === b) {
+      return 0;
+    }
+  });
+  renderList();
 }
-
 
 function addHandler() {
-    array.push('');
-    renderList();
+  array.push("");
+  renderList();
 }
-
 
 function createTaskElement(arrayEl, index) {
-    let block = document.createElement('div');
-    block.classList.add('task-block');
-    let input = document.createElement('input');
-    input.classList.add('task-input');
+  let block = document.createElement("div");
+  block.classList.add("task-block");
+  let input = document.createElement("input");
+  input.classList.add("task-input");
 
-    let xButton = document.createElement('button');
-    xButton.classList.add('x-button');
-    xButton.addEventListener('click', xButtonHandler);
+  let xButton = document.createElement("button");
+  xButton.classList.add("x-button");
+  xButton.addEventListener("click", xButtonHandler);
 
-    input.value = arrayEl;
-    input.id = index;
+  input.value = arrayEl;
+  input.id = index;
 
-    input.addEventListener('input', ((event) => {
-        let index = event.target.id;
-        let value = event.target.value;
-        array[index] = value;
-    }));
+  input.addEventListener("input", (event) => {
+    let index = event.target.id;
+    let value = event.target.value;
+    array[index] = value;
+  });
 
+  function xButtonHandler(event) {
+    let taskToDelete = event.target.previousElementSibling;
+    let parent = event.target.parentElement;
 
-    function xButtonHandler(event) {
-        let taskToDelete = event.target.previousElementSibling;
-        let parent = event.target.parentElement;
+    array = array.filter((item) => item != taskToDelete.value);
 
-        array = array.filter(item => item != taskToDelete.value)
+    if (array.length >= 1) {
+      parent.remove();
+    } else if ((array.length = 0)) {
+      // console.log(array);
+      // taskToDelete.value = null;
+    }
+  }
 
-        if (array.length >= 1) {
-            parent.remove();
-        } else if (array.length = 0) {
-            // console.log(array);
-            // taskToDelete.value = null;
-        }
-    };
-    
-    block.append(input, xButton);
-    return block
+  block.append(input, xButton);
+  return block;
 }
-
-
-
-
-
-// function xButtonHandler() {
-//     array = array.filter((item, index) => item != array[index]);
-//     block.remove()
-// };
-
-// let sortButtonCreate = (() => {
-//     sortingNew = document.createElement('div');
-//     sortingNew.classList.add('sorting');
-//     mainBlock.append(sortingNew);
-
-//     sortUpNew = document.createElement('button');
-//     sortUpNew.classList.add('sort-up');
-//     sorting.append(sortUpNew);
-// });
-
-// console.log(sortButtonCreate);
-// console.dir(sortButtonCreate);
-
-
-// function sortUpHandler () {
-//     otherTasks.innerHTML = '';
-
-//     data = array.sort((a,b) => b - a);
-
-//     data.forEach((item) => {
-//         otherTasks.append(createTaskElement(item));
-//     });
-// }
-
-
-// здесь мы запускаем forEach для каждого элемента массива array - и для каждого мы делаем:
-// tasker.append - подвязываем вниз элемента tasker (окошко с задачами) новый элемент.
-// (createTaskElement(item)) // это то, ЧТО мы подвязываем.
-// event.target.innerText = 'Нажала кнопку и меняю на ней текст';
-// event.target.style.backgroundColor = "aquamarine";
-// event.target.style.color = "black";
-
-
-// Пробовала как работает:
-// task.addEventListener('change', (event) => {
-//     console.log(event.target);
-//     console.log(event.target.value);
-// });
-
-
-// function clickHandler() {
-//     array.push(task.value);
-//     console.log(array);
-// }
-
-// Эта функция делает "отрисовку" таскера на странице при каждом запуске события - клика по кнопке "Добавить".
-
-
-
-// console.dir(addTaskButton);
-// Добавили обработчик события "клик" на кнопку "Добавить" (addTaskButton).
-// addTaskButton.addEventListener('click', clickHandler);
-
-
-
-// Что делает эта функция? эта функция и есть сам обработчик события, т.е. что происходит при клике на кнопку 
-// (что браузер делает при возникновении события).
-
-
-
-// data.forEach((item) => {
-    //     otherTasks.append(createTaskElement(item));
-    // });
-
-
-    // function xButtonHandler() {
-    //     console.log(array);
-    //     if (array.length > 1) {
-    //         array = array.filter(item => item != arrayEl);
-    //         block.remove();
-    //     } else if (array.length ===1) {
-    //         input.value = "";
-    //     }
-    // };
-
-
-
-
-    // if (array.length > 1) {
-    //     array = array.filter(item => item != arrayEl)
-    //     block.remove();
-    // } else if (array.length === 1) {
-    //     input.value = "";
-    // }
-
-
-    // array = array.filter((item,index) => {item[index] != input.value[input.id]});
-
-
-    // array = array.filter((item,index) => {
-    //     return item[index] != taskToDelete.value[taskToDelete.id];
-
-
-
-     // array = array.filter((item,index) => {
-        //     return item[index] != taskToDelete.value[taskToDelete.id];
-        //     // console.log(item, index)
-        //     // return item[index];
-        // });
-        // parent.remove();
-        // console.log(array);
-        // block.remove();
-
-
